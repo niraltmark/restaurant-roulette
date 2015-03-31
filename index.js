70ea161daffd38a9d7df2c3a4ead5c8655ba7656
Nir <niraltmark@gmail.com>
2015-03-31 21:00:46 +0300

    Integrating with GitHub

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var cool = require('cool-ascii-faces');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+
+app.get('/', function(request, response) {
+  response.send(cool());
+});
+
+//app.use(express.static(__dirname + '/public'));
+//
+//app.get('/', function(request, response) {
+//  response.send('Hello World!');
+//});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
