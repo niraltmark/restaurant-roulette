dd2641660b490ba7b4deb5e8526b9f30832d0265
Nir <niraltmark@gmail.com>
2015-04-02 10:03:35 +0300

    Updating server

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ var phridge  = require('phridge');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.use(express.static('www'));
 
-app.get('/', function(request, response) {
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+
+app.get('/restaurants/get', function(request, response) {
     phridge.spawn({ loadImages: false })
     .then(function (phantom) {
         return phantom.openPage("http://www.clickatable.co.il/");
@@ -58,52 +66,10 @@ app.get('/', function(request, response) {
         console.log(err);
     });
 });
-//        .then(function (phantom) {
-//            phantom.run("h1", function (selector, resolve) {
-//
-//                var page = webpage.create();
-//                page.open("http://www.clickatable.co.il/", function (status) {
-//                    page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-//                        page.evaluate(function() {
-//                            // Set my default values to Tel Aviv at 20:00 
-//                            $("#ctl00_ContentPlaceHolder1_searchTab1_ddlCities").val("b3b33667-84bb-443d-9908-bee86793d0f8");
-//                            $("#ctl00_ContentPlaceHolder1_searchTab1_ddlHour").val(20);
-//                            $("#ctl00_ContentPlaceHolder1_searchTab1_ddlMinutes").val(0);
-//
-//                            $("#ctl00_ContentPlaceHolder1_searchTab1_btnMultiSearch").click();
-//                        });
-//                        
-//                        // Waiting for something    
-//                        var intervalId = setInterval(function () {
-//                            var hasBeenFound = page.evaluate(function () {
-//                                return $("#progressDiv").is(":hidden");
-//                            });
-//
-//                            if (hasBeenFound === false) {
-//                                return;
-//                            }
-//
-//                            clearInterval(intervalId);
-//
-//                            if (hasBeenFound) {
-//                                resolve(page.evaluate(function(){ 
-//                                return $("#ctl00_ContentPlaceHolder1_UpdatePanel1 table tr.borderRow").length
-//                                    + $("#ctl00_ContentPlaceHolder1_UpdatePanel1 table tr.noBorderRow").length}));
-//                            }
-//                        }, 100);
-//                    });
-//                });
-//            });
-//        })
-//        .then(function (text) {
-//                // inside node again 
-//                response.send("Amount of results is " + text);
-//
-//                console.log("The element contains the following text: "+ text);
-//            });
-//        });
 
 // What is app.listen?
+app.set('port', (process.env.PORT || 5000));
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
