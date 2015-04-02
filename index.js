6556f1f18872d4128216f071088cdc71efba6314
Nir <niraltmark@gmail.com>
2015-04-02 13:47:17 +0300

    Add to the server side the restaurant fetcher

--- index.js
+++ index.js
@@ -15,65 +15,78 @@ app.all('*', function(req, res, next) {
 
 app.get('/restaurants/get', function(request, response) {
     
-    var cardTypes = [
-    { name: "גוצ'ה", image: 'https://pbs.twimg.com/profile_images/546942133496995840/k7JAxvgq.jpeg', time:"20:00" },
-    { name: "ג'ירף", image: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png',time:"19:00" },
-    { name: "צפרה", image: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg',time:"21:00" },
-  ];
+//    var cardTypes = [
+//    { name: "גוצ'ה", image: 'https://pbs.twimg.com/profile_images/546942133496995840/k7JAxvgq.jpeg', time:"20:00" },
+//    { name: "ג'ירף", image: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png',time:"19:00" },
+//    { name: "צפרה", image: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg',time:"21:00" },
+//  ];
+//    
+//    response.json(cardTypes);
     
-    response.json(cardTypes);
-    
-//    phridge.spawn({ loadImages: false })
-//    .then(function (phantom) {
-//        return phantom.openPage("http://www.clickatable.co.il/");
-//    })
-//    .then(function (page) {
-//        return page.run("h1", function (selector, resolve, reject) {
-//            // this function runs inside PhantomJS bound to the webpage instance
-//            
-//            var page = this;
-//
-//            page.evaluate(function () {
-//                $("#ctl00_ContentPlaceHolder1_searchTab1_ddlCities").val("b3b33667-84bb-443d-9908-bee86793d0f8").change();
-//                $("#ctl00_ContentPlaceHolder1_searchTab1_ddlHour").val(20);
-//                $("#ctl00_ContentPlaceHolder1_searchTab1_ddlMinutes").val(0);
-//
-//                $("#ctl00_ContentPlaceHolder1_searchTab1_btnMultiSearch").click();
-//            });
-//            
-//                var intervalId = setInterval(function () {
-//                var hasBeenFound = page.evaluate(function () {
-//                    return Boolean($("#progressDiv").is(":hidden"));
-//                });
-//
-//                if (hasBeenFound === false)
-//                    /* check if there is still some time left  */ {
-//                    // wait for next interval
-//                    
-//                    console.log("waiting...");
-//                    return;
-//                }
-//
-//                clearInterval(intervalId);
-//
-//                if (hasBeenFound) {
-//                    resolve(page.evaluate(function(){ 
-//                        return $("#ctl00_ContentPlaceHolder1_UpdatePanel1 table tr.borderRow").length
-//                            + $("#ctl00_ContentPlaceHolder1_UpdatePanel1 table tr.noBorderRow").length;
-//                    }));
-//                } else {
-//                    reject(new Error("Wait for timeout"));
-//                }
-//            }, 100);
-//        });
-//    })
-//    .then(function (result) {
-//        console.log(result);
-//        response.end("This is the result " + result);
-//    })
-//    .catch(function (err) {
-//        console.log(err);
-//    });
+    phridge.spawn({ loadImages: false })
+    .then(function (phantom) {
+        return phantom.openPage("http://www.clickatable.co.il/");
+    })
+    .then(function (page) {
+        return page.run("h1", function (selector, resolve, reject) {
+            // this function runs inside PhantomJS bound to the webpage instance
+            
+            var page = this;
+
+            page.evaluate(function () {
+                $("#ctl00_ContentPlaceHolder1_searchTab1_ddlCities").val("b3b33667-84bb-443d-9908-bee86793d0f8").change();
+                $("#ctl00_ContentPlaceHolder1_searchTab1_ddlHour").val(20);
+                $("#ctl00_ContentPlaceHolder1_searchTab1_ddlMinutes").val(0);
+
+                $("#ctl00_ContentPlaceHolder1_searchTab1_btnMultiSearch").click();
+            });
+            
+                var intervalId = setInterval(function () {
+                var hasBeenFound = page.evaluate(function () {
+                    return Boolean($("#progressDiv").is(":hidden"));
+                });
+
+                if (hasBeenFound === false)
+                    /* check if there is still some time left  */ {
+                    // wait for next interval
+                    
+                    console.log("waiting...");
+                    return;
+                }
+
+                clearInterval(intervalId);
+
+                if (hasBeenFound) {
+                    // Get the data
+                    
+                    var data = page.evaluate(function(){ 
+                       return $(".restaurantNameButton").map(function(index1,element1) {
+                           var name = $(element1).text();
+                           
+                           var times = $(element1).closest("tr").find(".btn_HazmenMakom1, .btn_HazmenMakom2").map(function(index2, element2){
+                               return $(element2).val();
+                            });
+                           
+                           return {name: name, time:times.get(0)};
+                       }).get();
+                    });
+                    
+                    resolve(data);
+                } else {
+                    reject(new Error("Wait for timeout"));
+                }
+            }, 100);
+        });
+    })
+    .then(function (result) {
+        console.log(result);
+        
+        //
+        response.json(result);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 });
 
 // What is app.listen?
